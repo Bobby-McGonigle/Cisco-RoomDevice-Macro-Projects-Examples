870ca2913abeb2d9e5f254f1d2c8efc2e0e41445
Bobby McGonigle <51381107+Bobby-McGonigle@users.noreply.github.com>
2020-05-07 16:43:53 -0400

    Update bootStatus.js

--- Macro Snippets/Boot Status Checker/bootStatus.js
+++ Macro Snippets/Boot Status Checker/bootStatus.js
@@ -3,16 +3,19 @@ const xapi = require('xapi');
 let secondsAlive;
 
 function bootStatus() {
-    var c = 1;
     xapi.status.get('SystemUnit Uptime').then((UpTime) => {
         secondsAlive = UpTime;
         if (secondsAlive >= 65) {
-            //run scripts you need after the macro environment crashes
+            /*
+            Run xApi you need HERE after the macro environment crashes
+            */
             console.log("Macro Script interrupted. Either script update or crash.");
             return;
         }
         do {
-            // Run scripts you need after a reboot
+            /*
+            Run xApi you need HERE after your system restarts
+            */
             return;
         }
         while (secondsAlive <= 64);
